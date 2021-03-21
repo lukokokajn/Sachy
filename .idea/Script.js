5e89dc810c2308d9dacf8fdc62c9f1521c3250a3
lukokokajn <lukas.novy@educanet.cz>
2021-03-21 22:26:41 +0100

    Done

---
+++ .idea/Script.js
@@ -0,0 +1,35 @@
+document.write(`
+<div class="pole">
+
+    <table>
+`)
+for (let i = 0; i < 4; i++) {
+    document.write(`
+<tr>
+    <td class="cerna"></td>
+    <td class="bila"></td>
+    <td class="cerna"></td>
+    <td class="bila"></td>
+    <td class="cerna"></td>
+    <td class="bila"></td>
+    <td class="cerna"></td>
+    <td class="bila"></td>
+</tr>
+<tr>
+    <td class="bila"></td>
+    <td class="cerna"></td>
+    <td class="bila"></td>
+    <td class="cerna"></td>
+    <td class="bila"></td>
+    <td class="cerna"></td>
+    <td class="bila"></td>
+    <td class="cerna"></td>
+</tr>
+`)
+
+}
+document.write(`
+    </table>
+    
+</div>
+`)
